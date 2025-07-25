5194a608a7ccc867ebcb3c2309d0beb954da7dc6
Tenpi <pie4chan@gmail.com>
2025-07-24 20:43:16 -0400

    win bat

---
+++ embed_binary.bat
@@ -0,0 +1,27 @@
+@echo off
+setlocal enabledelayedexpansion
+
+set BINARY_BUILDER=%USERPROFILE%\Documents\JUCE\extras\BinaryBuilder\Builds\VisualStudio2022\x64\Debug\ConsoleApp\BinaryBuilder.exe
+
+set PARAMETERS_JSON=processor\parameters.json
+set WEBVIEW_ZIP=build\webview_files.zip
+set OUTPUT_DIR=editor
+set CLASS_NAME=BinaryData
+
+if not exist "%OUTPUT_DIR%" (
+    mkdir "%OUTPUT_DIR%"
+)
+
+for /f "delims=" %%I in ('powershell -command "New-Item -ItemType Directory -Path ([System.IO.Path]::GetTempPath()) -Name ([System.Guid]::NewGuid().ToString())"') do set TMP_DIR=%%I
+
+copy "%PARAMETERS_JSON%" "%TMP_DIR%\"
+copy "%WEBVIEW_ZIP%" "%TMP_DIR%\"
+
+"%BINARY_BUILDER%" "%TMP_DIR%" "%OUTPUT_DIR%" "%CLASS_NAME%"
+
+rmdir /s /q "%TMP_DIR%"
+
+echo  - %OUTPUT_DIR%\%CLASS_NAME%.cpp
+echo  - %OUTPUT_DIR%\%CLASS_NAME%.h
+
+endlocal
