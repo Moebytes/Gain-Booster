643b15b8ee0f7d4314352f360930c14659d1fa48
Tenpi <pie4chan@gmail.com>
2025-10-02 06:02:08 -0400

    bin

--- editor/BinaryData.h
+++ editor/BinaryData.h
@@ -7,4 +7,10 @@ namespace BinaryData
     extern const char*  parameters_json;
     const int           parameters_jsonSize = 626;
 
+    extern const char*  presets_zip;
+    const int           presets_zipSize = 6479;
+
+    extern const char*  webview_files_zip;
+    const int           webview_files_zipSize = 3002900;
+
 }
